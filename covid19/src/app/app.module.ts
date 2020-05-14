5717cefb22d435008eb67e1ddcae020d7d9ef130
vmurali100 <vmurali100@gmail.com>
2020-05-14 22:22:17 +0530

    covid project

--- covid19/src/app/app.module.ts
+++ covid19/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardWidgetsComponent } from './dashboard-widgets/dashboard-widgets.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderNavComponent } from './header-nav/header-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardWidgetsComponent,
+    MainContentComponent,
+    ProjectsComponent,
+    NavComponent,
+    HeaderNavComponent
   ],
   imports: [
     BrowserModule
---
+++ covid19/src/app/dashboard-widgets/dashboard-widgets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-widgets',
+  templateUrl: './dashboard-widgets.component.html',
+  styleUrls: ['./dashboard-widgets.component.css']
+})
+export class DashboardWidgetsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
